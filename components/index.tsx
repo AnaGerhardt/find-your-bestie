5e9bc52c14559f447e8202d4ba0a0c4f4ad23baa
Ana <anacarolina.gerhardt@hotmail.com>
2021-01-10 03:04:06 -0300

    Adjustments to sidedrawer and double click on slider

--- components/index.tsx
+++ components/index.tsx
@@ -2,3 +2,4 @@
 export * from "./Footer/Footer";
 export * from "./Layout/Layout";
 export * from "./AutoSuggest/AutoSuggest";
+export * from "./ClientOnlyPortal/ClientOnlyPortal";
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,19 @@
 import "styles/globals.scss";
 import React from "react";
+import Router from "next/router";
 import { useRouter } from "next/router";
 import { Layout as BasicLayout } from "components";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 <meta
   name="viewport"
-  content="width=device-width, height=device-height,  initial-scale=1.0, user-scalable=no;user-scalable=0;"
-/>;
-{
-  /* <meta
-  name="viewport"
   content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-/>; */
-}
+/>;
+
+//Binding events.
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
